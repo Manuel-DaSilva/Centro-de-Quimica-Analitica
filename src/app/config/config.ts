6969fecc006ee9b7829e5b61b3b76c236cbed1ea
Manuel Da Silva <manueldasilva98@gmail.com>
2020-03-29 23:52:59 -0400

    corrected pipe

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,4 +2,5 @@
 export const URL_SERVICES = "https://intense-meadow-26325.herokuapp.com/";
 // ucodbspr_cqaclient 1p3712HSHS57S/2123NDB22
 // ucodbspr_cqaadmin 1p3712HSHS57S
+
 // cqa@cqa.com 123456a
